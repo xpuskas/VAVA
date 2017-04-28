041438fb691c03574cd9d578896c4fa7ea6bdc70
xpuskas <79718@is.stuba.sk>
2017-04-28 15:18:17 +0200

    Pridana verzia 0.01

---
+++ RPGames/src/application/Controller.java
@@ -0,0 +1,44 @@
+package application;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+public class Controller implements Initializable {
+
+	@FXML
+	ImageView image1;
+	@FXML
+	ImageView image2;
+	@FXML
+	ImageView image3;
+	@FXML
+	ImageView image4;
+	@FXML
+	ImageView image5;
+	@FXML
+	ImageView imageBig;
+	
+	
+	
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		Image solco = new Image("file:solco.jpg");
+		Image foto = new Image("file:foto.jpg");
+		
+		image1.setImage(foto);
+		image2.setImage(foto);
+		image3.setImage(foto);
+		image4.setImage(foto);
+		image5.setImage(foto);
+		
+		imageBig.setImage(solco);
+
+	}
+
+}
